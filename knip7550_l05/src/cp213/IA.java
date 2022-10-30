ebd60397ac1bbf2b385ac258b4754bebdfc053d7
KelliMK <kelli.m.knipe@gmail.com>
2022-10-30 18:38:44 -0400

    Initialized lab 5 and assignment 3

---
+++ knip7550_l05/src/cp213/IA.java
@@ -0,0 +1,13 @@
+package cp213;
+
+/**
+ * Inherited class in simple example of inheritance / polymorphism.
+ *
+ * @author David Brown
+ * @version 2022-02-25
+ */
+public class IA extends Student {
+
+	// your code here
+
+}
